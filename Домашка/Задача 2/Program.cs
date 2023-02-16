48b5cedd30f98ffd0ada23fb45ea482d98fbe281
nikita <tolstoguzov.nikita98@mail.ru>
2023-02-17 00:40:51 +0300

    задача 2

---
+++ Домашка/Задача 2/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 104, b = 26;
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+    Console.Write(" min = ");
+    Console.Write(b);
+}
+else
+{
+    Console.Write(" max = ");
+    Console.Write(b);
+    Console.Write(" min = ");
+    Console.Write(a);
+}
